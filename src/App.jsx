13cd054074301eaf8d8a7e32b69a012a6875e1d6
Miguel Osorio @Kelvur <Osorio10.92@gmail.com>
2022-08-14 18:28:42 +0200

    Fist commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// Core
+import React from 'react';
+// Styles
+import './App.css'
+
+
+function App(){
+    return (
+        <h1 className='ui-red'>Hello, world!</h1>
+    )
+}
+
+export default App
