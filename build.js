563f0f73f0941eab478faf8f398c9c59403c6e03
Miguel Osorio @Kelvur <Osorio10.92@gmail.com>
2022-08-15 16:05:14 +0200

    chore: install and configurate eslint

--- build.js
+++ build.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('esbuild').buildSync({
     entryPoints: ['src/index.tsx'],
     bundle: true,
     minify: true,
     sourcemap: true,
-    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
     outdir: 'dist',
 })
