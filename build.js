01a4591b046929793469cd39d1e774f19a86a41c
Miguel Osorio @Kelvur <Osorio10.92@gmail.com>
2022-12-10 01:10:14 +0100

    chore(build.js): extend the esbuild script to build so it allow watch mode, a server and a production build

--- build.js
+++ build.js
@@ -1,9 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('esbuild').buildSync({
+const esbuild = require('esbuild');
+
+
+const isProduction = process.env.NODE_ENV === 'production';
+const watch = process.env.WATCH === 'true';
+const server = process.env.SERVER === 'true';
+
+const buildConfig = {
     entryPoints: ['src/index.tsx'],
     bundle: true,
-    minify: true,
-    sourcemap: true,
+    watch, // Watch and server cannot be true in the same time
+    minify: isProduction,
+    sourcemap: !isProduction,
     target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
     outdir: 'dist',
-});
+};
+
+if(server){
+    esbuild.serve({
+        port: 3030,
+        servedir: 'dist',
+    }, buildConfig);
+} else {
+    esbuild.build(buildConfig);
+}
